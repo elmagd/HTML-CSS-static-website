8416e53633eba6d50024ce015c52e1558a0fa5fd
Majd <majd@git.com>
2019-03-02 21:52:47 +0200

    static html and css example

---
+++ java.js
@@ -0,0 +1,11 @@
+
+
+function clickMe() 
+{
+ user = document.myform.user.value;
+ pass = document.myform.pass.value;
+	if (user == "Muhammad"&& pass=="123") 
+	{ 
+		location.href="index.html"
+	}
+}
